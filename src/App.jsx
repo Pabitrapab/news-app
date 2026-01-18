756af11feb0b4925a3ca9f77cc684bacb866b6cb
Pabitrapab <pabitraparida1206@gmail.com>
2026-01-18 09:01:52 +0530

    fix layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import NewsCard from "./components/NewsCard";
 import newsData from "./data/newsData";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div style={{ maxWidth: "420px", margin: "auto" }}>
-      {newsData.map((news, index) => (
-        <NewsCard key={index} news={news} />
-      ))}
-    </div>
+    <>
+      <Navbar />
+
+      <div style={styles.container}>
+        {newsData.map((news, index) => (
+          <NewsCard key={index} news={news} />
+        ))}
+      </div>
+    </>
   );
 }
+
+const styles = {
+  container: {
+    maxWidth: "1100px",
+    margin: "0 auto",
+    padding: "15px",
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fit, minmax(280px, 1fr))",
+    gap: "15px"
+  }
+};
 
 export default App;
